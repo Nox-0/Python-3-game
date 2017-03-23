f05b8a454ddf2f649832fdfacade60e1585b36a7
bitlii <chenjcaki@gmail.com>
2017-03-23 16:37:37 +1300

    [NEW] Things
    >enemy.py to put enemies in.
    >items.py to put items in.

--- enemy.py
+++ enemy.py
@@ -11,8 +11,8 @@ class Enemy:
 
 class GreenSlime(Enemy):
     def __init__(self):
-        super().__init__(name="Green Slime", maxhealth=10, health=maxhealth, atk=5, weak="patk")
+        super().__init__(name="Green Slime", maxhealth=10, health=self.maxhealth, atk=5, weak="patk")
 
 class GoblinGuard(Enemy):
     def __init__(self):
-        super().__init__(name="Goblin Guard", maxhealth=20, health=maxhealth, atk=5, weak="patk")
+        super().__init__(name="Goblin Guard", maxhealth=20, health=self.maxhealth, atk=5, weak="patk")
